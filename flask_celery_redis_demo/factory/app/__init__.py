a048ff851aa1a865ba25efc8b5bda4d877df839f
aidenzdly <50066692+aidenzdly@users.noreply.github.com>
2020-04-09 15:49:57 +0800

    Add files via upload

---
+++ flask_celery_redis_demo/factory/app/__init__.py
@@ -0,0 +1,26 @@
+# coding;utf-8
+from flask import Flask
+from celery_app import celery
+from config import config
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .scheduled import scheduled as scheduled_blueprint
+    app.register_blueprint(scheduled_blueprint)
+
+    return app
+
+
+def make_celery(app):
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+    celery.Task = ContextTask
+    return celery
